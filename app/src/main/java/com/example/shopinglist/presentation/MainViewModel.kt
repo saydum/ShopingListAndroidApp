3ee430e8ac9d4660add16c8ce461715b9304fa51
Saydum Khalibekov <saydum.it5@gmail.com>
2022-12-11 13:09:57 +0300

    Implements clean architecture

---
+++ app/src/main/java/com/example/shopinglist/presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.shopinglist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shopinglist.data.ShopListRepositoryImpl
+import com.example.shopinglist.domain.DeleteShopItemUseCase
+import com.example.shopinglist.domain.EditShopItemUseCase
+import com.example.shopinglist.domain.GetShopListUseCase
+import com.example.shopinglist.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase =GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    var shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
